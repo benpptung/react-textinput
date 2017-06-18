dc372c8297a574710143b5b47a7481f14e17a82f
benpptung <benpptung@tacol.biz>
2017-06-18 22:07:05 +0800

    update

--- rendering.js
+++ rendering.js
@@ -36,6 +36,6 @@ function onChange(email) {
 
 function validator(text) {
   if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(text)) {
-    return 'it looks not like an invalid email';
+    return 'it looks not a valid email address';
   }
 }
