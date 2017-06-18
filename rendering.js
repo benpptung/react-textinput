730c4cc7a3dd728efac21d9a534b378fe62795bb
benpptung <benpptung@tacol.biz>
2017-06-18 21:58:32 +0800

    initial done

--- rendering.js
+++ rendering.js
@@ -13,6 +13,8 @@ var test = (
       <TextInput label="Email address"
                  message="we will email you a link to sign in"
                  type="email"
+                 onChange={onChange}
+                 validator={validator}
         />
     </div>
     <p></p>
@@ -25,4 +27,15 @@ if (mountnode) {
   ReactDOM.render(
     test,
     mountnode);
+}
+
+
+function onChange(email) {
+  console.log(email);
+}
+
+function validator(text) {
+  if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(text)) {
+    return 'it looks not like an invalid email';
+  }
 }
